3e4f4941463a4cabcf5ed2ef2d57179df307caf1
tvishnu3111 <145796147+tvishnu3111@users.noreply.github.com>
2026-07-05 22:55:59 +0530

    created all files for backend and frontend of ATS Checker project

---
+++ ats-checker-backend/src/main/java/com/atschecker/ats_checker_backend/AtsCheckerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atschecker.ats_checker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsCheckerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsCheckerBackendApplication.class, args);
+	}
+
+}
