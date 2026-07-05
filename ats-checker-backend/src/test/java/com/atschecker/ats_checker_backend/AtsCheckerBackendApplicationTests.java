3823eca8e757a049d1c3969a188e6670a3079a95
tvishnu3111 <145796147+tvishnu3111@users.noreply.github.com>
2026-07-05 22:58:46 +0530

    created furthur files also

---
+++ ats-checker-backend/src/test/java/com/atschecker/ats_checker_backend/AtsCheckerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atschecker.ats_checker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsCheckerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
