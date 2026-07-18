bb41285514b05b829525d7ad59f2040ea14bba88
tvishnu3111 <145796147+tvishnu3111@users.noreply.github.com>
2026-07-19 00:56:37 +0530

    updated contro; and service and added pdfbox to read pdf ans thing is this is not working for too uplode still changes required

--- ats-checker-backend/src/main/java/com/atschecker/ats_checker_backend/service/ResumeService.java
+++ ats-checker-backend/src/main/java/com/atschecker/ats_checker_backend/service/ResumeService.java
@@ -1,10 +1,11 @@
 package com.atschecker.ats_checker_backend.service;
 
 import com.atschecker.ats_checker_backend.dto.ATSResponse;
+import com.atschecker.ats_checker_backend.util.PdfUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Arrays;
+import java.util.*;
 
 @Service
 public class ResumeService {
@@ -12,26 +13,32 @@ public class ResumeService {
     public ATSResponse analyzeResume(MultipartFile resume,
                                      String jobDescription) {
 
+        String resumeText = PdfUtil.extractText(resume);
+
+        System.out.println("=========== RESUME ===========");
+        System.out.println(resumeText);
+
         ATSResponse response = new ATSResponse();
 
         response.setScore(80);
 
-        response.setMatchedSkills(
-                Arrays.asList("Java", "Spring Boot")
-        );
+        response.setMatchedSkills(Arrays.asList(
+                "Java",
+                "Spring Boot"
+        ));
 
-        response.setMissingSkills(
-                Arrays.asList("Docker", "AWS")
-        );
+        response.setMissingSkills(Arrays.asList(
+                "Docker",
+                "AWS"
+        ));
 
-        response.setSuggestions(
-                Arrays.asList(
-                        "Mention Docker",
-                        "Add AWS Project"
-                )
-        );
+        response.setSuggestions(Arrays.asList(
+                "Learn Docker",
+                "Mention AWS Projects"
+        ));
 
         return response;
+
     }
 
 }
--- ats-checker-backend/src/main/java/com/atschecker/ats_checker_backend/util/PdfUtil.java
+++ ats-checker-backend/src/main/java/com/atschecker/ats_checker_backend/util/PdfUtil.java
@@ -1,5 +1,33 @@
 package com.atschecker.ats_checker_backend.util;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public class PdfUtil {
-    
+
+    public static String extractText(MultipartFile file) {
+
+        try {
+
+            PDDocument document = Loader.loadPDF(file.getBytes());
+
+            PDFTextStripper stripper = new PDFTextStripper();
+
+            String text = stripper.getText(document);
+
+            document.close();
+
+            return text;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
 }
