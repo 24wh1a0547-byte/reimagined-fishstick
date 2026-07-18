202118642710db39d6c63eea4c00f72e6e846c58
tvishnu3111 <145796147+tvishnu3111@users.noreply.github.com>
2026-07-19 00:48:31 +0530

    updated controller and service java codes and json is is providing my dummy data through json

--- ats-checker-backend/src/main/java/com/atschecker/ats_checker_backend/controller/ResumeController.java
+++ ats-checker-backend/src/main/java/com/atschecker/ats_checker_backend/controller/ResumeController.java
@@ -1,14 +1,25 @@
 package com.atschecker.ats_checker_backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.atschecker.ats_checker_backend.dto.ATSResponse;
+import com.atschecker.ats_checker_backend.service.ResumeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/api/resume")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ResumeController {
 
-    @GetMapping("/api/test")
-    public String test() {
-        return "ATS Backend is Working!";
-    }
+    @Autowired
+    private ResumeService resumeService;
 
+    @PostMapping("/analyze")
+    public ATSResponse analyzeResume(
+            @RequestParam("resume") MultipartFile resume,
+            @RequestParam("jobDescription") String jobDescription
+    ) {
+
+        return resumeService.analyzeResume(resume, jobDescription);
+    }
 }
--- ats-checker-backend/src/main/java/com/atschecker/ats_checker_backend/service/ResumeService.java
+++ ats-checker-backend/src/main/java/com/atschecker/ats_checker_backend/service/ResumeService.java
@@ -1,5 +1,37 @@
 package com.atschecker.ats_checker_backend.service;
 
+import com.atschecker.ats_checker_backend.dto.ATSResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
+
+@Service
 public class ResumeService {
-    
+
+    public ATSResponse analyzeResume(MultipartFile resume,
+                                     String jobDescription) {
+
+        ATSResponse response = new ATSResponse();
+
+        response.setScore(80);
+
+        response.setMatchedSkills(
+                Arrays.asList("Java", "Spring Boot")
+        );
+
+        response.setMissingSkills(
+                Arrays.asList("Docker", "AWS")
+        );
+
+        response.setSuggestions(
+                Arrays.asList(
+                        "Mention Docker",
+                        "Add AWS Project"
+                )
+        );
+
+        return response;
+    }
+
 }
